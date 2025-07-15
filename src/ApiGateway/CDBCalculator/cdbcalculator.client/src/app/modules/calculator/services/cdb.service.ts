e528776f976ad255705fefbc60b228373a993473
Weslley Murdock <weslleyluiz@outlook.com.br>
2025-07-15 03:48:50 -0300

    fix up vs runs

--- src/ApiGateway/CDBCalculator/cdbcalculator.client/src/app/modules/calculator/services/cdb.service.ts
+++ src/ApiGateway/CDBCalculator/cdbcalculator.client/src/app/modules/calculator/services/cdb.service.ts
@@ -2,18 +2,24 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { CdbSimulationResponse, CdbSimulationRequest } from '../models/cdb.model';
+import { environment } from '../../../../environments/environment';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class CdbService {
+  private readonly baseUrl = `${environment.apiUrl}/gateway/Cdb`
   constructor(private readonly http: HttpClient) { }
 
   simulate(data: CdbSimulationRequest): Observable<CdbSimulationResponse> {
     const formData = new FormData();
     formData.append('Months', data.months.toString());
     formData.append('InitialValue', data.initialValue.toString());
-
-    return this.http.post<CdbSimulationResponse>('/gateway/Cdb/Simulate', formData);
+    return this.http.post<CdbSimulationResponse>(
+      `${environment.apiUrl}/Cdb/Simulate`,
+      formData
+    );
   }
+
 }
