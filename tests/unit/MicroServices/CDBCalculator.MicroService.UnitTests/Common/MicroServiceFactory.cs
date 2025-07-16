dd2c8cd494e06f28a45385ab25424eeecd317a5f
Weslley Murdock <weslleyluiz@outlook.com.br>
2025-07-16 01:23:46 -0300

    refactor factory

--- tests/unit/MicroServices/CDBCalculator.MicroService.UnitTests/Common/MicroServiceFactory.cs
+++ tests/unit/MicroServices/CDBCalculator.MicroService.UnitTests/Common/MicroServiceFactory.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.Hosting;
 
@@ -8,20 +9,16 @@ public static class MicroServiceFactory
 {
     public static HttpClient CreateClient(string environment = "Development")
     {
-        var contentRoot = Path.GetFullPath(Path.Combine(
-            AppContext.BaseDirectory,
-            "..", "..", "..", "..", "..", "..", "..",
-            "src", "MicroServices", "CDBCalculator"
-        ));
+        var factory = new WebApplicationFactory<Program>()
+            .WithWebHostBuilder(builder =>
+            {
+                builder.UseEnvironment(environment);
+                builder.UseSolutionRelativeContentRoot("src/ApiGateway/CDBCalculator/CDBCalculator.Server");
+            });
 
-        var builder = new WebHostBuilder()
-            .UseEnvironment(environment)
-            .UseContentRoot(contentRoot)
-            .UseStartup<Startup>();
-
-        var server = new TestServer(builder);
-        return server.CreateClient();
+        return factory.CreateClient();
     }
+
     public static IHost CreateHost(string environment = "Development")
     {
         return Program.CreateHostBuilder([$"--environment={environment}"])
